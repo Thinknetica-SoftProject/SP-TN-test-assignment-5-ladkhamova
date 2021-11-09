46d65305870fcfd460ee9130987dedd78006750e
ladkhamova <93874517+ladkhamova@users.noreply.github.com>
2021-11-09 11:06:21 +0300

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -10,4 +10,12 @@
 #
 ## Решение:
 
-
+numbers = []
+i = 0
+code = 0
+File.open("data/3.txt", 'r').each_line do |line|
+  numbers << line.split.map(&:to_i)
+  code += numbers[i].max - numbers[i].min
+  i += 1
+end
+puts code
